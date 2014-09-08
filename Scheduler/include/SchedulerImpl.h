d48f470da71a5e58aa2c7bcf1d24a4e6348c0e96
teams <teams@scheduler.localdomain>
2014-09-08 17:57:28 -0300

    Initial code for the Schedule module.

---
+++ Scheduler/include/SchedulerImpl.h
@@ -0,0 +1,43 @@
+#ifndef SchedulerImpl_H
+#define SchedulerImpl_H
+
+#include "SchedulerS.h"
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+#include <acscomponentImpl.h>
+#include <SchedulerS.h>
+
+
+class Scheduler: public acscomponent::ACSComponentImpl,
+	    public virtual POA_SCHEDULER_MODULE::Scheduler
+{
+  public:
+    /**
+     * Constructor
+     * 
+     * @param name component's name. This is also the name that will be used to find the
+     * configuration data for the component in the Configuration Database.
+     * @param containerServices The pointer to the ContainerServices
+     */
+    Scheduler(
+	 const ACE_CString& name,
+	 maci::ContainerServices * containerServices);
+
+    /**
+     * Destructor
+     */
+    virtual ~Scheduler();
+
+    virtual void start (void);
+
+    virtual void stop (void);
+
+    virtual ::CORBA::Long proposalUnderExecution (void);
+ 
+};
+
+
+#endif /*!SchedulerImpl_H*/
