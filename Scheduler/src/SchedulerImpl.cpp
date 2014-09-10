5ee45d822ec0be6db238091c9d5256eecc3773b7
teams <teams@scheduler.localdomain>
2014-09-10 11:21:15 -0300

    Work thread improvements.
    - Keep components as private, and declare SchedulerThread as friend.
    - Increase thread timeout to 10 s
    - Create the thread at execute(), resume it start() and suspend it at stop()

--- Scheduler/src/SchedulerImpl.cpp
+++ Scheduler/src/SchedulerImpl.cpp
@@ -13,8 +13,6 @@ Scheduler::Scheduler (
 
 Scheduler::~Scheduler ()
 {
-    if (m_schedulerLoop_p != NULL)
-	getContainerServices()->getThreadManager()->destroy(m_schedulerLoop_p);
 }
 
 void Scheduler::execute()
@@ -33,6 +31,10 @@ void Scheduler::execute()
 
   if (database_p == DATABASE_MODULE::DataBase::_nil() )
     ACS_SHORT_LOG((LM_ERROR,"Scheduler::execute: cant retrieve component DataBase"));
+
+  Scheduler * selfPtr = this;
+  m_schedulerLoop_p = getContainerServices()->getThreadManager()->
+                      create<SchedulerThread, Scheduler*>("schedulerLoop", selfPtr);
 }
 
 
@@ -54,28 +56,40 @@ void Scheduler::cleanUp()
     }
     // Here we have to stop all threads
     getContainerServices()->getThreadManager()->stopAll();
+    if (m_schedulerLoop_p != NULL) {
+      getContainerServices()->getThreadManager()->destroy(m_schedulerLoop_p);
+      m_schedulerLoop_p = NULL;
+    }
 }
 
 void Scheduler::start (void)
 {
   ACS_SHORT_LOG((LM_INFO,"Scheduler::start"));
-  if (m_schedulerLoop_p == NULL) {
-    Scheduler * selfPtr = this;
-    m_schedulerLoop_p = getContainerServices()->getThreadManager()->
-                        create<SchedulerThread, Scheduler*>("schedulerLoop", selfPtr);
-    m_schedulerLoop_p->resume();
-  } else
-    ACS_SHORT_LOG((LM_INFO,"Scheduler::start: already started"));
+  if (m_schedulerLoop_p != NULL) {
+    // Restart observations by resuming the work thread
+    // If it is already running, give an error (it means we were called twice).
+    if (m_schedulerLoop_p->isSuspended()) {
+      ACS_SHORT_LOG((LM_INFO,"Scheduler::start: Resume work thread."));
+      m_schedulerLoop_p->resume();
+    } else
+      ACS_SHORT_LOG((LM_ERROR,"Scheduler::start: work thread is already running"));
+  }  else
+    ACS_SHORT_LOG((LM_ERROR,"Scheduler::start: work thread not started"));
 }
 
 void Scheduler::stop (void)
 {
   ACS_SHORT_LOG((LM_INFO,"Scheduler::stop"));
   if (m_schedulerLoop_p != NULL) {
-    getContainerServices()->getThreadManager()->destroy(m_schedulerLoop_p);
-    m_schedulerLoop_p = NULL;
+    if (!m_schedulerLoop_p->isSuspended()) {
+      // Pause observations by suspending the work thread
+      // If it is already suspended, give an error (it means we were called twice).
+      ACS_SHORT_LOG((LM_INFO,"Scheduler::stop: Suspend work thread."));
+      m_schedulerLoop_p->suspend();
+    } else
+      ACS_SHORT_LOG((LM_ERROR,"Scheduler::start: work thread is already running"));
   } else
-    ACS_SHORT_LOG((LM_INFO,"Scheduler::stop: not started"));
+    ACS_SHORT_LOG((LM_ERROR,"Scheduler::stop: work thread not started"));
 }
 
 ::CORBA::Long Scheduler::proposalUnderExecution (void)
