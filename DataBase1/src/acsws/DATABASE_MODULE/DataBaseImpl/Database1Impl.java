ea25321b4ee1c9acfc5149cdeaae64efd3f79e1c
DataBase1 <database1@acsws.alma.cl>
2016-08-11 18:02:49 +0000

    Several tools finished

--- DataBase1/src/acsws/DATABASE_MODULE/DataBaseImpl/Database1Impl.java
+++ DataBase1/src/acsws/DATABASE_MODULE/DataBaseImpl/Database1Impl.java
@@ -1,6 +1,12 @@
 package acsws.DATABASE_MODULE.DataBaseImpl;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import org.eclipse.uml2.uml.RaiseExceptionAction;
+
 import acsws.DATABASE_MODULE.DataBaseOperations;
 import acsws.SYSTEMErr.ImageAlreadyStoredEx;
 import acsws.SYSTEMErr.InvalidProposalStatusTransitionEx;
@@ -22,7 +28,9 @@ public class Database1Impl implements DataBaseOperations, ComponentLifecycle {
 	private Logger m_logger;
 	int ind = STATUS_INITIAL_PROPOSAL;
 	public ArrayList<Proposal> propArray = new ArrayList<Proposal>();
+	public ArrayList<byte[][]> imagArray = new ArrayList<byte[][]>();
 	acsws.STORAGE_MODULE.Storage storage;
+	int firstPid;
 	
 	
 	//public Database1Impl() {	
@@ -37,20 +45,29 @@ public class Database1Impl implements DataBaseOperations, ComponentLifecycle {
 		} catch (AcsJContainerServicesEx e) {
 			m_logger.warning(e.toString());
 		}
+		storage.clearAllData();
 	}
 	public ComponentStates componentState() {
 		ComponentStates cs =  m_containerServices.getComponentStateManager().getCurrentState();
 		return cs;
 	}
 	public int storeProposal (Target[] target){
+		firstPid = storage.getNextValidId();
 		Proposal prop;
 		int ind = propArray.size();
 		prop = new Proposal(ind, target, 0);
 		propArray.add(prop);
-		//seeProp();
+		byte[][] emptyImage = new byte[target.length][];
+		imagArray.add(emptyImage);
 		return prop.pid;
 	}
 	public int getProposalStatus(int pid) throws ProposalDoesNotExistEx{
+		if (pid > propArray.size()){
+			throw new ProposalDoesNotExistEx();
+		}
+		if (propArray.get(pid).status == -1){
+			throw new ProposalDoesNotExistEx();
+		}
 		return propArray.get(pid).status;
 	}
 	public void removeProposal(int pid) {
@@ -88,9 +105,22 @@ public class Database1Impl implements DataBaseOperations, ComponentLifecycle {
 		return tempPropArray;
 	}
 	public void setProposalStatus(int pid, int status) throws InvalidProposalStatusTransitionEx,ProposalDoesNotExistEx {
-		if(propArray.get(pid).status != -1){
+		if (pid > propArray.size()){
+			throw new ProposalDoesNotExistEx();
+		}
+		if (propArray.get(pid).status == -1){
+			throw new ProposalDoesNotExistEx();
+		}
+		if(propArray.get(pid).status == -1){
 			m_logger.warning("Proposal number "+ pid + " doesn't exist");
 		}else{
+			int currentStat = propArray.get(pid).status;
+			if (currentStat == 0 && status != 1){
+				throw new InvalidProposalStatusTransitionEx();
+			}
+			if (currentStat == 1 && status != 2){
+				throw new InvalidProposalStatusTransitionEx();
+			}
 			propArray.get(pid).status = status;
 			String status_str = "";
 			switch(status){
@@ -108,21 +138,57 @@ public class Database1Impl implements DataBaseOperations, ComponentLifecycle {
 		}
 	}
 	public void storeImage(int pid, int tid, byte[] image) throws ImageAlreadyStoredEx,ProposalDoesNotExistEx {
-		byte[][] imagesStorage = storage.getObservation(pid);
-		imagesStorage[tid] = image;
-		storage.storeObservation(propArray.get(pid), imagesStorage);
+		if (pid > propArray.size()){
+			throw new ProposalDoesNotExistEx();
+		}
+		if (propArray.get(pid).status == -1){
+			throw new ProposalDoesNotExistEx();
+		}
+		int tarLen = propArray.get(pid).targets.length;
+		byte[][] element = imagArray.get(pid);
+		if (element[tid] == null){
+			element[tid] = image;
+		} else{
+			throw new ImageAlreadyStoredEx();
+		}
+		imagArray.add(pid, element);
+		boolean complete = true;
+		for (int i = 0; i<tarLen; i++){
+			if (element[i] == null){
+				complete = false;
+				break;
+			}
+		}
+		if (complete == true){
+			storage.storeObservation(propArray.get(pid), element);
+		}
 	}
 	public byte[][] getProposalObservations(int pid) throws ProposalNotYetReadyEx {
+		byte[][] element = imagArray.get(pid);
+		int tarLen = propArray.get(pid).targets.length;
+		boolean exception = false;
+		for (int i = 0; i<tarLen; i++){
+			if (element[i] == null){
+				exception = true;
+				break;
+			}
+		}
+		if (exception == true){
+			throw new ProposalNotYetReadyEx();
+		}
 		byte[][] imagesStorage = storage.getObservation(pid);
 		return imagesStorage;
 	}
 	
 	
 	public void execute() throws ComponentLifecycleException {
-		
+		m_logger.info("execute() called...");
 	}
 	public void cleanUp() throws AcsJComponentCleanUpEx {
-		
+//		clean();
+//		Called after the last functional call to the component has finished. 
+//		The component should then orderly release resources etc.
+		m_containerServices.releaseComponent("STORAGE");
 	}
 	public void aboutToAbort() {
 		
@@ -132,12 +198,16 @@ public class Database1Impl implements DataBaseOperations, ComponentLifecycle {
 		return m_containerServices.getName();
 	}
 	
-	
-	
-
-
-	
 	public void clean() {
+		if(propArray.isEmpty()== true){
+			m_logger.warning("Proposals already clean");
+		}else{
+			for(int i=0;i<propArray.size();i++){
+				propArray.remove(i);
+			}
+			m_logger.info("All proposals clean");
+		}
+		
 		
 	}
 
